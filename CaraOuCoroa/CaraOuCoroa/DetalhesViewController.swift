7495dc4afbb8440f09cf05a3893a418341894e01
Núbio Knupp <nubioknupp@gmail.com>
2016-11-13 11:05:27 -0200

    Crie layout e navegação do app

--- CaraOuCoroa/CaraOuCoroa/DetalhesViewController.swift
+++ CaraOuCoroa/CaraOuCoroa/DetalhesViewController.swift
@@ -1,25 +1,23 @@
 //
-//  ViewController.swift
+//  DetalhesViewController.swift
 //  CaraOuCoroa
 //
 //  Created by Nubio Knupp on 23/10/16.
 //  Copyright © 2016 Nubio Knupp. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class DetalhesViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
